50de82bd6a4ec04ebad0d081b3ed1417b71f25ab
Asur <78565438+dxASURxd@users.noreply.github.com>
2024-01-15 20:31:45 -0600

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="h-20 w-full bg-[#572364] p-4 flex justify-between text-white items-center">
+      <Link to="/">
+        <span>FakeStore</span>
+      </Link>
+      {/* <h2>Numero de items:</h2> */}
+      <Link to="/carrito">
+        <span>Carrito</span>
+      </Link>
+    </nav>
+  );
+};
+
+export default NavBar;
